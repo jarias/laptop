e6594fa311ae4497c40800fe90970e87f88fdedf
Julio Arias <jarias01@gmail.com>
2018-06-21 15:39:59 -0600

    refactor arch install scripts

--- scripts/arch-install-chroot.sh
+++ scripts/arch-install-chroot.sh
@@ -4,6 +4,7 @@ set -e
 
 USERNAME=jarias
 USER_FULLNAME="Julio Arias"
+HOSTNAME=gildarts
 
 ln -sf /usr/share/zoneinfo/America/Costa_Rica /etc/localtime
 
@@ -18,7 +19,7 @@ echo "gildarts" >/etc/hostname
 cat <<EOF >/etc/hosts
 127.0.0.1 localhost
 ::1       localhost
-127.0.1.1 gildarts
+127.0.1.1 ${HOSTNAME}
 EOF
 
 cat <<EOF >/etc/vconsole.conf
@@ -37,60 +38,6 @@ FILES=()
 HOOKS=(base udev autodetect keyboard keymap consolefont modconf block encrypt filesystems fsck)
 EOF
 
-cat <<EOF >/etc/default/grub
-# GRUB boot loader configuration
-GRUB_DEFAULT=0
-GRUB_TIMEOUT=5
-GRUB_DISTRIBUTOR="Arch"
-GRUB_CMDLINE_LINUX_DEFAULT="quiet"
-GRUB_CMDLINE_LINUX="cryptdevice=/dev/nvme0n1p3:cryptroot"
-
-# Preload both GPT and MBR modules so that they are not missed
-GRUB_PRELOAD_MODULES="part_gpt part_msdos"
-
-# Uncomment to enable booting from LUKS encrypted devices
-#GRUB_ENABLE_CRYPTODISK=y
-
-# Uncomment to enable Hidden Menu, and optionally hide the timeout count
-#GRUB_HIDDEN_TIMEOUT=5
-#GRUB_HIDDEN_TIMEOUT_QUIET=true
-
-# Uncomment to use basic console
-GRUB_TERMINAL_INPUT=console
-
-# Uncomment to disable graphical terminal
-#GRUB_TERMINAL_OUTPUT=console
-
-# The resolution used on graphical terminal
-# note that you can use only modes which your graphic card supports via VBE
-# you can see them in real GRUB with the command 'vbeinfo'
-GRUB_GFXMODE=auto
-
-# Uncomment to allow the kernel use the same resolution used by grub
-GRUB_GFXPAYLOAD_LINUX=keep
-# Uncomment if you want GRUB to pass to the Linux kernel the old parameter
-# format "root=/dev/xxx" instead of "root=/dev/disk/by-uuid/xxx"
-#GRUB_DISABLE_LINUX_UUID=true
-
-# Uncomment to disable generation of recovery mode menu entries
-GRUB_DISABLE_RECOVERY=true
-
-# Uncomment and set to the desired menu colors.  Used by normal and wallpaper
-# modes only.  Entries specified as foreground/background.
-#GRUB_COLOR_NORMAL="light-blue/black"
-#GRUB_COLOR_HIGHLIGHT="light-cyan/blue"
-
-# Uncomment one of them for the gfx desired, a image background or a gfxtheme
-#GRUB_BACKGROUND="/path/to/wallpaper"
-#GRUB_THEME="/path/to/gfxtheme"
-
-# Uncomment to get a beep at GRUB start
-#GRUB_INIT_TUNE="480 440 1"
-# Uncomment to make GRUB remember the last selection. This requires to
-# set 'GRUB_DEFAULT=saved' above.
-#GRUB_SAVEDEFAULT="true"
-EOF
-
 systemctl enable sshd.service
 systemctl enable ntpd.service
 systemctl enable NetworkManager.service
@@ -104,6 +51,7 @@ echo 'root ALL=(ALL) ALL' >/etc/sudoers
 echo '%wheel ALL=(ALL) ALL' >>/etc/sudoers
 echo '#includedir /etc/sudoers.d' >>/etc/sudoers
 
+#default password 123
 useradd -c "$USER_FULLNAME" \
   -m \
   -s /bin/bash \
--- scripts/arch-install.sh
+++ scripts/arch-install.sh
@@ -2,27 +2,32 @@
 
 set -e
 
+ROOT_DEVICE=/dev/nvme0n1
+ESP_PARTITION=${ROOT_DEVICE}p1
+BOOT_PARTITION=${ROOT_DEVICE}p2
+ROOT_PARTITION=${ROOT_DEVICE}p3
+
 timedatectl set-ntp true
 
-parted -s /dev/nvme0n1 mklabel gpt
-parted -s -a optimal /dev/nvme0n1 mkpart ESP fat32 1MiB 551MiB
-parted -s /dev/nvme0n1 set 1 esp on
-parted -s -a optimal /dev/nvme0n1 mkpart boot ext4 551MiB 651MiB
-parted -s -a optimal /dev/nvme0n1 mkpart primary ext4 651MiB 100%
+parted -s $ROOT_DEVICE mklabel gpt
+parted -s -a optimal $ROOT_DEVICE mkpart ESP fat32 1MiB 551MiB
+parted -s $ROOT_DEVICE set 1 esp on
+parted -s -a optimal $ROOT_DEVICE mkpart boot ext4 551MiB 651MiB
+parted -s -a optimal $ROOT_DEVICE mkpart primary ext4 651MiB 100%
 
-mkfs.vfat -F 32 /dev/nvme0n1p1
-mkfs.ext4 -F -L boot /dev/nvme0n1p2
+mkfs.vfat -F -n esp 32 $ESP_PARTITION
+mkfs.ext4 -F -L boot $BOOT_PARTITION
 
-cryptsetup -y -v luksFormat --type luks2 /dev/nvme0n1p3
-cryptsetup open /dev/nvme0n1p3 cryptroot
+cryptsetup -y -v luksFormat --type luks2 $ROOT_PARTITION
+cryptsetup open $ROOT_PARTITION cryptroot
 mkfs.xfs -f -L rootfs /dev/mapper/cryptroot
 mount /dev/mapper/cryptroot /mnt
 
 mkdir /mnt/boot
-mount /dev/nvme0n1p2 /mnt/boot
+mount $BOOT_PARTITION /mnt/boot
 
 mkdir /mnt/boot/EFI
-mount /dev/nvme0n1p1 /mnt/boot/EFI
+mount $ESP_PARTITION /mnt/boot/EFI
 
 pacstrap /mnt \
   base \
@@ -45,6 +50,8 @@ pacstrap /mnt \
   git
 
 genfstab -U /mnt >>/mnt/etc/fstab
+
+sed -i "s@GRUB_CMDLINE_LINUX=.*@GRUB_CMDLINE_LINUX=\"cryptdevice=${ROOT_PARTITION}:cryptroot\"@g" /mnt/etc/default/grub
 
 cp arch-install-chroot.sh /mnt
 
