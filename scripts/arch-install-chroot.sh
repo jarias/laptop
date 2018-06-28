9f246a315c325156aa6f831a5a1d0ef8645283b7
Julio Arias <jarias01@gmail.com>
2018-06-27 21:39:10 -0600

    use HOSTNAME var

--- scripts/arch-install-chroot.sh
+++ scripts/arch-install-chroot.sh
@@ -14,7 +14,7 @@ echo en_US.UTF-8 UTF-8 >/etc/locale.gen
 echo LANG=en_US.UTF-8 >/etc/locale.conf
 locale-gen
 
-echo "gildarts" >/etc/hostname
+echo "${HOSTNAME}" >/etc/hostname
 
 cat <<EOF >/etc/hosts
 127.0.0.1 localhost
